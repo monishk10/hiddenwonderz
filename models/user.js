18640b580f0d2cc075c19fa2099a172f6ff96418
Monish Kapadia <monishk10@yahoo.com>
2018-01-06 16:06:05 +0530

    userModel: Added more arguments

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
+	name: String,
+	lastName: String,
+	email: String,
     username: String,
     password: String
 });
