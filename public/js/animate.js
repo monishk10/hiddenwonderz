d47453f6dbfe3edbcfeb0563cbc646351d5bd8af
Monish Kapadia <monishk10@yahoo.com>
2018-01-21 13:02:55 +0530

    Anime-text: Switched to minimalistic

--- public/js/animate.js
+++ public/js/animate.js
@@ -3,13 +3,20 @@ $('.anime-text .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
-anime.timeline({loop: false})
-  .add({
-    targets: '.anime-text .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 1200,
-    delay: function(el, i) {
-      return 50 * i;
-    }
-  });
+$(".index-header").hover(
+  function() {
+    anime.timeline({loop: false})
+    .add({
+      targets: '.anime-text .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 1200,
+      delay: function(el, i) {
+        return 50 * i;
+      }
+    });
+  },
+  // While exiting
+  function() {
+  }
+);
