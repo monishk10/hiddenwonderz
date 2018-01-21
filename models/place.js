89265544aadf0251c5d8b281f1b3ce549469268b
Monish Kapadia <monishk10@yahoo.com>
2018-01-21 13:47:10 +0530

    Maps: Added Google maps

--- models/place.js
+++ models/place.js
@@ -6,6 +6,9 @@ var placeSchema = new mongoose.Schema({
    description: String,
    createdAtDate: String,
    createdAtTime: String,
+   location: String, 
+   lat: Number, 
+   lng: Number,
    author: {
       id: {
          type: mongoose.Schema.Types.ObjectId,
